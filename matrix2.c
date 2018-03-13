6b1a7c3b972f6796d208abf5dc0741e3373d814f
Panagiotis Zouggourides <pansot11@gmail.com>
2018-03-13 10:56:25 +0200

    Update matrix2.c

--- matrix2.c
+++ matrix2.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
-// compile like:  gcc -Wall -O2 -DNROWS=10000 matrix2.c -o matrix2
+// compile like:  gcc -Wall -O2 -DNROWS=10000 matrix1.c -o matrix1
 
 
 #define NCOLS 100
@@ -17,7 +17,7 @@ void get_walltime(double *wct) {
 int main() {
 double *table;
 double ts,te;
-
+int i,j;
 
   table = (double *)malloc(NROWS*NCOLS*sizeof(double)); 
   if (table==NULL) {
@@ -27,24 +27,39 @@ double ts,te;
 
   // warmup
 
-  // ...your code here...
+  // ...your code here..
+	for (i=0; i<NROWS*NCOLS; i++) {
+		table[i]=1.0;
+	}
 
   // get starting time (double, seconds) 
   get_walltime(&ts);
   
-  // workload
+  // workload ana grammi                                  ana grammi
+	for (i=0; i<NCOLS; i++) {
+		for (j=0; j<NROWS; j++) {
+			table[i*NROWS +j]=table[i*NROWS +j]+13;              
+		}	
+	} 
 
   // ...your code here...
 
-  // get ending time
+  // get ending time  
   get_walltime(&te);
 
   // check results
   
   // ...your code here...
-
+	for (i=0; i<NROWS*NCOLS; i++) {
+			if (table[i]!=14.0) { //an den einai iso me to arxiko+13 pou tou edwsa 
+				printf("There was a problem!");		
+			}
+			
+			
+	}
   // print time elapsed and/or Maccesses/sec
-  
+  double timepassed= te-ts; //orizo timepassed=end-start
+	printf("\n\ntimepassed: %lf\n",timepassed);		
   // ...your code here...  
   
   free(table);
